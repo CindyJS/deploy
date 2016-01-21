fa5621bf80fc113bd589c8b35420ce94ea971cd8
Travis CI <travis-ci@cinderella.de>
2016-01-21 08:43:10 +0000

    Build of CindyJS 0666386

--- Cindy.js
+++ Cindy.js
@@ -1,6 +1,6 @@
 /* CindyJS - (C) 2014-2015  The CindyJS Project
  * Mostly licensed under the Apache License 2.0, but subprojects may use different licensing.
- * See https://github.com/CindyJS/CindyJS/tree/659a88df602e91253da4433d13e886f7c61e0b2d
+ * See https://github.com/CindyJS/CindyJS/tree/0666386042ff4f8fef899ba5d57ff6e001fb0f87
  * for corresponding sources and their respective licensing conditions.
  */
 'use strict';(function(){function r(b,c,d){f.biginteger_used=1;null!=b&&("number"==typeof b&&"undefined"==typeof c?this.fromInt(b):"number"==typeof b?this.fromNumber(b,c,d):null==c&&"string"!=typeof b?this.fromString(b,256):this.fromString(b,c))}function F(){return new r(null)}function Y(b,c,d,h,f,l){for(;0<=--l;){var x=c*this[b++]+d[h]+f;f=Math.floor(x/67108864);d[h++]=x&67108863}return f}function Ma(b,c,d,h,f,l){var x=c&32767;for(c>>=15;0<=--l;){var p=this[b]&32767,r=this[b++]>>15,D=c*p+r*x,p=x*
